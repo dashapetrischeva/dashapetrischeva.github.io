2f277e7448e6d97e296945399e66788b090a3535
dashapetrischeva <151852045+dashapetrischeva@users.noreply.github.com>
2025-09-02 03:50:42 +0200

    Add files via upload
    js бьет

---
+++ script/script.js
@@ -0,0 +1,48 @@
+"use strict"
+
+window.addEventListener('load', windowLoad)
+
+
+function windowLoad() {
+
+
+
+	sliderInit();
+}
+
+
+
+
+
+
+function sliderInit() {
+	const sliderReviews = new Swiper('.slider-works', {
+		// Optional parameters
+		loop: true,
+		slidesPerView: 3,
+		spaceBetween: 24,
+		breakpoints: {
+			320: {
+				slidesPerView: 1.1,
+				spaceBetween: 10,
+			},
+			600: {
+				slidesPerView: 1.4,
+				spaceBetween: 15,
+			},
+			768: {
+				slidesPerView: 2,
+				spaceBetween: 20,
+			},
+			1050: {
+				slidesPerView: 3,
+				spaceBetween: 24,
+			},
+		},
+
+
+
+
+
+	});
+}
